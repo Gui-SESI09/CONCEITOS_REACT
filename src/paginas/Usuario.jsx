0fd6df6db05c5888be0d9c10a5deac1b8f584dde
GUILHERME DALSASSO MACHADO <guilherme_machado18@estudante.sesisenai.org.br>
2026-05-19 12:21:20 -0300

    Use State: email, nome e senha

--- src/paginas/Usuario.jsx
+++ src/paginas/Usuario.jsx
@@ -2,8 +2,11 @@ import { useEffect, useState } from "react"
 
 export default function Usuario () {
     const [contador, setContador] = useState(0)
+    const [email, setEmail] = useState('')
+    const [nome, setNome] = useState('')
+    const [senha, setSenha] = useState('')
     const [usuarios, setUsuarios] = useState([])
-    
+   
     useEffect(() => {
         document.title = 'Contagem ' + contador
         const buscarUsuarios = async () => {
@@ -22,6 +25,39 @@ export default function Usuario () {
             <h1>{contador}</h1>
             
             <button onClick={ () => {setContador(contador + 1)} }>Somar</button>
+            
+            <br /> <br />
+
+            <input type="text" id="email" 
+            placeholder="DIGITE EMAIL"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+
+            />
+            
+            <h3>{email}</h3>
+            
+            <br /> <br />
+
+            <input type="text" id="email" 
+            placeholder="DIGITE O SEU NOME"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+
+            />
+            
+            <h3>{nome}</h3>
+            
+            <br /> <br />
+
+            <input type="text" id="email" 
+            placeholder="DIGITE A SUA SENHA"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+
+            />
+            
+            <h3>{senha}</h3>
 
             <ul>
             {usuarios.map( (usuario) => (
